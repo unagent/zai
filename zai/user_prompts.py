882018ff72149c956bd613737f7e81333cfab0b5
Krystian Z <krystian.zawistowski4@gmail.com>
2025-04-26 12:51:02 +0200

    feat: add user prompts module

---
+++ zai/user_prompts.py
@@ -0,0 +1,13 @@
+import glob,os
+
+def list_prompts(directory):
+    prompt_paths = glob.glob(directory + '/*txt')
+
+    all_prompts = []
+    for v in prompt_paths:
+        with open(v,'r') as fp:
+            content = fp.read()
+        name = os.path.basename(v).replace('.txt','')
+        # ai! 
+
+
